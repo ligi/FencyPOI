643138cbbd998bce7e85039ef777e3a70941751c
Andreas Schildbach <andreas@schildbach.de>
2013-06-08 21:24:47 +0200

    add content provider

---
+++ FencyPOI/src/main/java/org/battlehack/fencypoi/POIDBContentProvider.java
@@ -0,0 +1,82 @@
+package org.battlehack.fencypoi;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+
+import java.util.List;
+
+/**
+ * Created by aschildbach on 6/8/13.
+ */
+public class POIDBContentProvider extends ContentProvider {
+
+    public static final String TABLE_NAME = "POIS";
+    public static final Uri CONTENT_URI = Uri.parse("content://org.battlehack.fencypoi." + TABLE_NAME);
+    public static final String KEY_LAT = "LAT";
+    public static final String KEY_LON = "LON";
+    public static final String KEY_NAME = "NAME";
+    public static final String KEY_RADIUS = "RADIUS";
+    public static final String KEY_ALTITUDE = "ALTITUDE";
+    public static final String KEY_DESCRIPTION = "DESCRIPTION";
+    public static final String KEY_CREATED = "CREATED";
+    public static final String KEY_TYPE = "TYPE";
+    private POIDBCreateHelper helper;
+
+    @Override
+    public boolean onCreate() {
+        helper = new POIDBCreateHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public String getType(final Uri uri) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Uri insert(final Uri uri, final ContentValues values) {
+        long rowId = helper.getWritableDatabase().replace(TABLE_NAME, null, values);
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return ContentUris.withAppendedId(CONTENT_URI, rowId);
+    }
+
+    @Override
+    public int update(final Uri uri, final ContentValues values, final String selection, final String[] selectionArgs) {
+        final int count = helper.getWritableDatabase().update(TABLE_NAME, values, selection, selectionArgs);
+
+        if (count > 0)
+            getContext().getContentResolver().notifyChange(uri, null);
+
+        return count;
+    }
+
+    @Override
+    public int delete(final Uri uri, final String selection, final String[] selectionArgs) {
+
+        final int count = helper.getWritableDatabase().delete(TABLE_NAME, selection, selectionArgs);
+
+        if (count > 0)
+            getContext().getContentResolver().notifyChange(uri, null);
+
+        return count;
+    }
+
+    @Override
+    public Cursor query(final Uri uri, final String[] projection, final String selection, final String[] selectionArgs, final String sortOrder) {
+        final List<String> pathSegments = uri.getPathSegments();
+
+        final SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        qb.setTables(TABLE_NAME);
+
+        final Cursor cursor = qb.query(helper.getReadableDatabase(), projection, selection, selectionArgs, null, null, sortOrder);
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        return cursor;
+    }
+}
--- FencyPOI/src/main/java/org/battlehack/fencypoi/POIDBCreateHelper.java
+++ FencyPOI/src/main/java/org/battlehack/fencypoi/POIDBCreateHelper.java
@@ -8,31 +8,20 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by ligi on 6/8/13.
  */
 public class POIDBCreateHelper extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 1;
 
     private static final String DATABASE_NAME = "DBNAME";
 
-    private static final String KEY_LAT = "LAT";
-    private static final String KEY_LON = "LON";
-    private static final String KEY_NAME = "NAME";
-    private static final String KEY_RADIUS = "RADIUS";
-    private static final String KEY_ALTITUDE = "ALTITUDE";
-    private static final String KEY_DESCRIPTION = "DESCRIPTION";
-    private static final String KEY_CREATED = "CREATED";
-    private static final String KEY_TYPE = "TYPE";
-
-    private static final String DICTIONARY_TABLE_NAME = "dictionary";
-
     private static final String DICTIONARY_TABLE_CREATE =
-            "CREATE TABLE " + DICTIONARY_TABLE_NAME + " (" +
-                    KEY_LAT + " INTEGER, " +
-                    KEY_LON + " INTEGER," +
-                    KEY_ALTITUDE + " INTEGER," +
-                    KEY_NAME + " TEXT," +
-                    KEY_RADIUS + " TEXT," +
-                    KEY_DESCRIPTION + " TEXT," +
-                    KEY_CREATED + " CREATOR," +
-                    KEY_TYPE + " TYPE);";
+            "CREATE TABLE " + POIDBContentProvider.TABLE_NAME + " (" +
+                    POIDBContentProvider.KEY_LAT + " INTEGER, " +
+                    POIDBContentProvider.KEY_LON + " INTEGER," +
+                    POIDBContentProvider.KEY_ALTITUDE + " INTEGER," +
+                    POIDBContentProvider.KEY_NAME + " TEXT," +
+                    POIDBContentProvider.KEY_RADIUS + " TEXT," +
+                    POIDBContentProvider.KEY_DESCRIPTION + " TEXT," +
+                    POIDBContentProvider.KEY_CREATED + " CREATOR," +
+                    POIDBContentProvider.KEY_TYPE + " TYPE);";
 
     POIDBCreateHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
