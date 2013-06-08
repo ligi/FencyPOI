2b5407f729c9976ce27edee4a69943329f714f7b
ligi <ligi@ligi.de>
2013-06-08 17:17:58 +0200

    sketch adding of fences

--- FencyPOI/src/main/java/org/battlehack/fencypoi/MainActivity.java
+++ FencyPOI/src/main/java/org/battlehack/fencypoi/MainActivity.java
@@ -3,13 +3,20 @@ package org.battlehack.fencypoi;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.add_fence);
+        Spinner spinner = (Spinner) findViewById(R.id.type_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, new String[]{"Power Outlet", "Apple Tree", "Danger Zone"});
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+
     }
 
 
