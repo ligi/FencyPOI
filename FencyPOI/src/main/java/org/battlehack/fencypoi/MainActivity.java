3464554020a77e576c978d3659c02cec0c479415
ligi <ligi@ligi.de>
2013-06-08 19:09:38 +0200

    get location from fused location provider

--- FencyPOI/src/main/java/org/battlehack/fencypoi/MainActivity.java
+++ FencyPOI/src/main/java/org/battlehack/fencypoi/MainActivity.java
@@ -1,22 +1,44 @@
 package org.battlehack.fencypoi;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+
+public class MainActivity extends Activity implements GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener,LocationListener {
+
+    private LocationClient locationclient;
+    private TextView locationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        setupLocationListener();
+
         setContentView(R.layout.add_fence);
         Spinner spinner = (Spinner) findViewById(R.id.type_spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, new String[]{"Power Outlet", "Apple Tree", "Danger Zone"});
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         spinner.setAdapter(adapter);
 
+        locationEditText=(TextView) findViewById(R.id.location_edittext);
+    }
+
+    private void setupLocationListener() {
+        locationclient = new LocationClient(this,this,this);
+        locationclient.connect();
     }
 
 
@@ -26,5 +48,26 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        LocationRequest locationrequest = LocationRequest.create();
+        locationrequest.setInterval(100);
+        locationclient.requestLocationUpdates(locationrequest, this);
+    }
+
+    @Override
+    public void onDisconnected() {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        locationEditText.setText(location.toString());
+    }
 }
