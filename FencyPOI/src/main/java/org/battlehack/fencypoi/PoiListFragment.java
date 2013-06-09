3ece5290b5398e5c2c41369c425ba7944ab7e6e6
ligi <ligi@ligi.de>
2013-06-09 11:12:06 +0200

    delete on longclick

--- FencyPOI/src/main/java/org/battlehack/fencypoi/PoiListFragment.java
+++ FencyPOI/src/main/java/org/battlehack/fencypoi/PoiListFragment.java
@@ -4,7 +4,11 @@ import android.R;
 import android.app.ListFragment;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.BaseColumns;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 /**
@@ -12,12 +16,31 @@ import android.widget.SimpleCursorAdapter;
  */
 public class PoiListFragment extends ListFragment {
 
+    private Cursor cursor;
+
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final Cursor cursor = getActivity().managedQuery(POIDBContentProvider.CONTENT_URI, null, null, null, null);
+        cursor = getActivity().managedQuery(POIDBContentProvider.CONTENT_URI, null, null, null, null);
 
         setListAdapter(new SimpleCursorAdapter(getActivity(), R.layout.simple_list_item_2, cursor, new String[]{POIDBContentProvider.KEY_NAME, POIDBContentProvider.KEY_DESCRIPTION}, new int[]{android.R.id.text1, android.R.id.text2}));
+
+    }
+
+    @Override
+    public void onResume() {
+
+        super.onResume();
+
+        getListView().setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                cursor.move(i);
+                String where = BaseColumns._ID + "='" + cursor.getInt(cursor.getColumnIndexOrThrow(BaseColumns._ID)) + "'";
+                getActivity().getContentResolver().delete(POIDBContentProvider.CONTENT_URI, where,null );
+                return false;
+            }
+        });
     }
 }
